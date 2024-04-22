2ff8358217781bb459e977bf42e33f03584f4ee8
G10 <G10@PROELE72>
2024-04-22 14:48:32 +0200

    Add project files.

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleApp1
+{
+    internal class Person
+    {
+        public Person(string navn, DateTime birth)
+        {
+            _navn = navn;
+            _birth = birth;
+
+        }
+        
+        private string _navn;
+        private DateTime _birth;
+
+
+        public void NavnCount()
+        {
+            Console.WriteLine(_navn);
+            Console.WriteLine(_navn.Length);
+            
+        }
+        public void GetAge()
+        {
+            var today = DateTime.Today;
+            var age = today.Year - _birth.Year;
+
+
+            if (_birth.Date > today.AddYears(-age)) age--;
+
+            for (int i = age; i >= 0; i--)
+            {
+                Console.WriteLine($"Year {i}: {_birth.Year + i}");
+            }
+
+            Console.WriteLine(age);
+        }
+
+        public void GetAgeArray()
+        {
+            var age = DateTime.Today.Year - _birth.Year;
+            int[] ageCount = new int[age];            
+            
+            for (int i = 0; i < age; i++)
+            {
+                ageCount[i] = i;
+                
+            }
+            foreach (var item in ageCount)
+            {
+                Console.WriteLine($"Age: {item}");
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name  = Console.ReadLine();
+            int year = Convert.ToInt32(Console.ReadLine());
+            int month = Convert.ToInt32(Console.ReadLine());
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            DateTime birth = new DateTime(year, month, day);
+
+            Person obj = new Person(name, birth);
+
+    
+
+            obj.NavnCount();
+            obj.GetAgeArray();
+            //obj.GetAge();//
+
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
